f98bdcce7164ba8b02109e8a3a0a27f6a7ac94c8
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-05-10 23:10:19 +0300

    ввод массива с клавиатуры

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите количество элементов массива");
+int len = Convert.ToInt32(Console.ReadLine());
+
+
+// ввод массива с клавиатуры
+
+String[] CreateAndFillArray(int len)
+{
+    string[] array = new string[len];
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
