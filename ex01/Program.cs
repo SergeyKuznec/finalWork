67f74dbe5c27f7502469e35749622eedde9ee200
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-05-11 21:03:58 +0300

    решение задачи

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -5,12 +5,46 @@ int len = Convert.ToInt32(Console.ReadLine());
 
 // ввод массива с клавиатуры
 
-String[] CreateAndFillArray(int len)
-{
-    string[] array = new string[len];
+ String[] array = new String[len];
     for (int i = 0; i < len; i++)
     {
-        array[i] = Convert.ToString(Console.ReadLine());
+        array[i] = Console.ReadLine(); 
     }
-    return array;
+// Печать массива
+ void PrintArray(string[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
 }
+
+// создание массива из валюдных элементов
+String[] ValidArray(String[] array)
+{
+    int len = array.Length;
+    int number = 0;
+    for (int index = 0; index < len; index++)
+    {
+        if (array[index].Length <= 3)
+        {
+            number++;
+        }
+    }
+    String[] arrayResult = new String[number];
+    int i = 0; 
+    for (int index = 0; index < len; index++)
+    {
+        if (array[index].Length <= 3)
+        {
+            arrayResult[i++] = array[index];
+        }
+    }
+    return arrayResult;
+}
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+String[] valid = ValidArray(array);
+PrintArray(valid);
